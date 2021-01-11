3aee2268b80e79b9efaa96c77af39dd42af3b156
Tóth Sándor <52709221+SanyiToth@users.noreply.github.com>
2021-01-11 18:26:24 +0100

    1.feladat

---
+++ feladat1.js
@@ -0,0 +1,8 @@
+let x = window.prompt("Adj meg egy tetszőleges számot!");
+if (x > 0) {
+    console.log("A szám pozitív!");
+} else if (x == 0) {
+    console.log("nulla");
+} else {
+    console.log("A szám negatív");
+}
