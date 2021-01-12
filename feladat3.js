fa52a97fa913afae98050a3a4902bdb46e23fb3a
”Tóth <sandor.toth@hotmail.com>
2021-01-12 18:40:19 +0100

    small changes feladat3

--- feladat3.js
+++ feladat3.js
@@ -2,7 +2,7 @@ let c = null;
 let n = null;
 while (n == null) {
     let a = Number.parseInt(window.prompt("Adj meg egy tetszőleges számot!"));
-    let b = Number.parseInt(window.prompt("Adj meg egy tetszőleges számot!"));
+    let b = Number.parseInt(window.prompt("Adj meg egy másik tetszőleges számot!"));
     let operator = window.prompt("Add meg milyen müveletet végezzünk el a két számon (+,-,/,*)? ");
     if (operator == "+") {
         c = a + b;
