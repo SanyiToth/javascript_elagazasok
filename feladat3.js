5d70c9a061e42510686dbc3a2e48b05bf8654f6b
”Tóth <sandor.toth@hotmail.com>
2021-01-12 18:35:03 +0100

    feladat3 with while loop

--- feladat3.js
+++ feladat3.js
@@ -1,22 +1,29 @@
-let a = Number.parseInt(window.prompt("Adj meg egy tetszőleges számot!"));
-let b = Number.parseInt(window.prompt("Adj meg egy tetszőleges számot!"));
 let c = null;
-let operator = window.prompt("Add meg milyen müveletet végezzünk el a két számon (+,-,/,*)? ");
-if (operator == "+") {
-    c = a + b;
-    console.log("A két szám összege: " + c);
-} else if (operator == "-") {
-    c = a - b;
-    console.log("A két szám különbsége: " + c);
-} else if (operator == "/") {
-    c = a / b;
-    console.log("A két szám hányadosa: " + c);
-} else if (operator == "*") {
-    c = a * b;
-    console.log("A két szám szorzata: " + c);
-} else {
-    c = "Hibás műveletet adtál meg!";
-    console.log(c);
+let n = null;
+while (n == null) {
+    let a = Number.parseInt(window.prompt("Adj meg egy tetszőleges számot!"));
+    let b = Number.parseInt(window.prompt("Adj meg egy tetszőleges számot!"));
+    let operator = window.prompt("Add meg milyen müveletet végezzünk el a két számon (+,-,/,*)? ");
+    if (operator == "+") {
+        c = a + b;
+        console.log("A két szám összege: " + c);
+        n++;
+    } else if (operator == "-") {
+        c = a - b;
+        console.log("A két szám különbsége: " + c);
+        n++;
+    } else if (operator == "/") {
+        c = a / b;
+        console.log("A két szám hányadosa: " + c);
+        n++;
+    } else if (operator == "*") {
+        c = a * b;
+        console.log("A két szám szorzata: " + c);
+        n++;
+    } else {
+        c = "Hibás műveletet adtál meg!";
+        window.alert(c);
+    }
 }
 
 
